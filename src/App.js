e0af7fe931cdf3eca1891a093d1768f0e3e2660e
Daniel Anton <vasquevasque1114@gmail.com>
2021-04-05 14:25:28 -0400

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./componets/Navbar"
 
 function App() {
   return (
-    <h1>Portfolio Build...</h1>
+    <Navbar/>
   );
 }
 
